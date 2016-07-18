02bc361072c9edec4e2a43dadea5345ef3c4f8bb
sheepforce <phillip.seeber@gmx.de>
2016-07-18 18:07:10 +0200

    Improving two important routines in chemistry module, making them much faster
    	- XYZ.hs: reduce IO calls in getGeometriesFromTraj and trajAtomCoordList

--- modules/Chemistry/XYZ.hs
+++ modules/Chemistry/XYZ.hs
@@ -39,11 +39,21 @@ from the same atom in every block -}
 trajAtomLines :: Int -> Int -> [Int]
 trajAtomLines numberOfAtoms atom = [atom + 2 + a | a <- [0,(numberOfAtoms + 2)..]]
 
+
 {- takes a trajectory, an atom to monitor and the number of steps in the trajectory
 and returns a list of coordinates for this atom in each MD step -}
+-- old version, SLOW
+{-
 trajAtomCoordList :: String -> Int -> Int -> [[Float]]
 trajAtomCoordList content atom mdSteps = [getCoordFromLine content a | a <- b]
   where b = take mdSteps (trajAtomLines (nAtoms content) atom)
+-}
+trajAtomCoordList :: String -> Int -> Int -> [[Float]]
+trajAtomCoordList content atom mdSteps = map (take 3) d
+  where a = lines content
+	b = map words [(a!!(x-1)) | x <- (take mdSteps (trajAtomLines (nAtoms content) atom))]
+	c = map tail b
+	d = map (map (read :: String -> Float)) c
 
 {- takes a xyz file and tells the element symbol of an atom -}
 getElement :: String -> Int -> String
@@ -52,16 +62,29 @@ getElement content atom = head a
         b = c!!(atom + 1)
         c = lines content
 
- {- getGeometriesFromTraj IS A COMPLICATED FUNCTION. It takes a simple XYZ trajectory content and a number
- of mdSteps as input and returns a [[[Float]]]. The outer layer of this list is the mdStep, the middle layer
- is indexing the atoms in this mdStep and the inner layer is the actual coordinate of this atom -}
+{- getGeometriesFromTraj IS A COMPLICATED FUNCTION. It takes a simple XYZ trajectory content and a number
+of mdSteps as input and returns a [[[Float]]]. The outer layer of this list is the mdStep, the middle layer
+is indexing the atoms in this mdStep and the inner layer is the actual coordinate of this atom -}
+-- old version, SLOW
+{-
 getGeometriesFromTraj :: String -> Int -> [[[Float]]]
 getGeometriesFromTraj content mdSteps = a
   where a = [[getCoordFromLine content stepLine | stepLine <- (c!!b)] | b <- [0..(mdSteps - 1)]]
 	c = take mdSteps [[(1 + 2 + a)..(2 + a + d)] | a <- [0,(d + 2)..]]
 	d = nAtoms content
+-}
+getGeometriesFromTraj :: String -> Int -> [[[Float]]]
+getGeometriesFromTraj content mdSteps = map (map (map (read :: String -> Float))) g
+  where a = lines content
+	b = map words a
+	c = [[(b!!(d - 1)) | d <- (y!!e)] | e <- [0..(mdSteps - 1)]]
+	f = map (map tail) c
+	g = map (map (take 3)) f
+	y = take mdSteps [[(1 + 2 + a)..(2 + a + z)] | a <- [0,(z + 2)..]]
+	z = nAtoms content
 
 
+	
 {- ############ -}
 {- IO functions -}
 {- ############ -}
